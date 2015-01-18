0cf431b29555634d009ee5ab8f71acd47c32a4fe
student <student@stedent-G750JX>
2015-01-18 17:33:21 -0600

    First Commit in the starter application.

---
+++ tests/unit/example.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+describe("example", function(){
+   
+   it("1 + 1 = 2", function(){
+       expect(1 + 1).toBe(2);
+   });
+});
